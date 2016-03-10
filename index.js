cc65a005aa5a7d9d0efb9768efdc1148f134f3fe
davidjwalter <dwalter@1043labs.com>
2016-03-10 10:35:10 -0500

    Add version test

--- index.js
+++ index.js
@@ -2,8 +2,16 @@
 
 var git = require('git-rev');
 var async = require('async');
-
 var cmds = ['branch', 'tag', 'short', 'long'];
+var version;
+
+try {
+    version = require('package.json').version;
+} catch (e) {
+    version = 'none';
+}
+
+console.log('version='+version);
 
 async.eachSeries(cmds, function (cmd, callback) {
     git[cmd](function (str) {
@@ -11,3 +19,4 @@ async.eachSeries(cmds, function (cmd, callback) {
         callback();
     });
 });
+
